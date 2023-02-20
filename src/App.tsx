7a167318d238a261383e55706450030b3614f478
Kremix1 <suttles123654@gmail.com>
2023-02-20 16:19:06 +0500

    Файлы проекта

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss'
+import './null.scss'
+import {Register} from "./pages/Register";
+import {Login} from "./pages/Login";
+import {Home} from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
